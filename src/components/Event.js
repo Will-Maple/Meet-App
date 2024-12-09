4dab5b12dd692bbbbc3354ae488e67d442d938ae
Will-Maple17 <will.maple17@gmail.com>
2024-12-09 14:13:11 -0600

    Update added integration test and oauth

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,6 +1,18 @@
-const Event = () => {
+import React, { useState } from 'react';
+
+const Event = ({ event }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
   return (
-    <li></li>
+    <li>
+      <h3>{event.summary}</h3>
+      <p>{event.created}</p>
+      <p>{event.location}</p>
+      <button onClick={() => setShowDetails(!showDetails)}>
+        {showDetails ? 'hide details' : 'show details'}
+      </button>
+      {showDetails && <p>{event.description}</p>}
+    </li>
   );
 }
 
